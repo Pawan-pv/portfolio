72625f92dbb9a13f65e85b3e32b7e3cb676f1e7b
pawan <pawanwarhade36@gmail.com>
2024-09-07 13:01:13 +0530

    modify some code

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,18 +1,58 @@
-import {  Roboto } from 'next/font/google'
-import Nav from './components/Nav';
-import HomeSec from "../my-components/HeroSec"
- 
-const roboto = Roboto({ weight:"700" , subsets:["cyrillic"]})
+import { Roboto } from 'next/font/google'
+import Nav from '../my-components/Nav';
+import HomeProfile from "../my-components/HeroSec"
+
+const roboto = Roboto({ weight: "700", subsets: ["cyrillic"] })
 
 export default function Home() {
-  return (<div className={`${roboto.className} bg-[#222831] h-screen text-yellow-300 pl-3 pr-3 md:pr-20  md:pl-20`}>
+  return (<div className={`${roboto.className} bg-slate-900 min-h-screen text-yellow-300 px-3 md:px-20`}>
+    {/* bg-[#222831]  */}
     
-    <div className="border">
-     <Nav/>
-     
-     <br />
-     <HomeSec/>
+    <Nav />
+  
+    <div className="pt-8 space-y-8">
+      {/* This is the small white box (likely a decorative element) */}
+      <div className="bg-white p-[3px] max-w-44 rounded-sm"></div>
+  
+      {/* Introduction Section */}
+      <div className="space-y-3">
+        <h1 className="text-4xl">I’m John, a Web Developer</h1>
+        <p className="text-gray-300">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus nihil error magni praesentium sit optio! Saepe tempora temporibus laboriosam vel officiis deserunt ipsum nostrum corrupti suscipit, omnis obcaecati quasi nam?
+        </p>
+      </div>
+  
+      {/* Blue circle or decorative element */}
+      <div className="bg-blue-600 p-12 w-20 h-20 rounded-full"></div>
+  
+      {/* About Me Section */}
+      <div className="flex space-x-4">
+        <div className="space-y-4">
+          <h3 className="text-xl">ABOUT ME</h3>
+          <p className='text-gray-300' >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non molestias labore velit, necessitatibus esse ut ad porro nam quam molestiae maxime obcaecati quisquam distinctio, tenetur laborum. Distinctio earum nihil eveniet?
+          </p>
+         <div>
+         <span className=" text-xl border-b-4 pb-3 border-b-gray-400">LEARN MORE</span>
+         </div>
+        </div>
+        <div className="space-y-4">
+          <h3 className="text-xl">ABOUT ME</h3>
+          <p className='text-gray-300' >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non molestias labore velit, necessitatibus esse ut ad porro nam quam molestiae maxime obcaecati quisquam distinctio, tenetur laborum. Distinctio earum nihil eveniet?
+          </p>
+         <div>
+         <span className=" text-xl border-b-4 pb-3 border-b-gray-400">LEARN MORE</span>
+         </div>
+        </div>
+      </div>
+       <div className='bg-slate-950 border-t-2 border-t-gray-700 pt-6'>
+        {/* HomeSec Component */}
+       <HomeProfile />
+       </div>
     </div>
   </div>
+  
+
   );
 }
--- my-app/src/my-components/HeroSec.tsx
+++ my-app/src/my-components/HeroSec.tsx
@@ -1,9 +1,31 @@
 "use client"
 import React from 'react'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+// import Image from 'next/image';
 
 const HeroSec = () => {
   return (
-    <div className="text-3xl bg-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam id qui odit voluptates numquam possimus repellendus ipsum et assumenda pariatur, minus, velit tempore expedita temporibus tenetur alias earum atque! Officiis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, enim impedit! Adipisci delectus, commodi praesentium molestias ducimus voluptate! Perspiciatis in necessitatibus maiores corrupti modi rem quia quaerat voluptas soluta aspernatur!</div>
+    <>
+      <div className=''>
+        <div className='space-y-2'>
+          <h3 className="text-xl">FOLLOW ME</h3>
+          <div className='space-x-4  border pl-3'>
+            <FacebookIcon   color="primary"/>
+            <TwitterIcon  color="primary"/>
+            <InstagramIcon  color="primary"/>
+            <LinkedInIcon  color="primary"/>
+            <GitHubIcon  color="primary"/>
+          </div>
+          {/* <Image>
+            
+          </Image> */}
+        </div>
+      </div>
+    </>
   )
 }
 
